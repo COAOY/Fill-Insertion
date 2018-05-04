f2f2b99dbd752b4bdd570e9bd7ad6b342b2e6dc6
ChritineLai <chritinelai@localhost.localdomain>
2018-05-04 12:32:59 -0400

    I've write the parser parsing the config file, input circuit file and the rule file. But the process file is not yet done.

---
+++ Parser.cpp
@@ -0,0 +1,161 @@
+/********************
+parser.cpp
+********************/
+#include "circuit.h"
+#include <string>
+
+void circuit::parser(char* _filename)
+{
+////////////////////////////////////////////////////////////////////////////////////// Parsing input config file
+
+	string design;
+	string output;
+	string rulefile;
+	string processfile;
+
+	fstream FileInCong;
+	FileInCong.open (argv[1], ios::in);
+
+	if (FileInCong)
+		cout << "Input Config file open success." << endl;
+	else
+		cout << "Input Config file open FAILED." << endl;
+
+	int k = 100;
+	char temp[k];
+
+	FileInCong.getline(temp, k, ' ');
+	FileInCong >> design;
+
+	FileInCong.getline(temp, k, ' ');
+	FileInCong >> output;
+
+	FileInCong.getline(temp, k, ' ');
+	FileInCong >> rulefile;
+
+	FileInCong.getline(temp, k, ' ');
+	FileInCong >> processfile;
+	
+	string t;
+	int a;
+
+	while(FileInCong >> t)
+	{
+		if (t == "critical_nets:")
+		{
+			FileInCong >> t;
+			while (t != "power_nets:")
+			{
+				a = atoi(t.c_str());
+				_critical.push_back(a);
+				FileInCong >> t;
+			}
+
+			FileInCong >> t;
+			while (t != "ground_nets:")
+			{
+				a = atoi(t.c_str());
+				_power.push_back(a);
+				FileInCong >> t;
+			}
+
+			FileInCong >> t;
+			while (!FileInCong.eof())
+			{
+				a = atoi(t.c_str());
+				_ground.push_back(a);
+				FileInCong >> t;
+			}
+		}
+	}
+
+	FileInCong.close();
+////////////////////////////////////////////////////////////////////////////////////// Parsing the layout input
+
+	cout << "===========================================================" << endl;
+
+	fstream FileInDes;
+	FileInDes.open (design, ios::in);
+
+	if (FileInDes)
+		cout << "Reading......" << endl;
+	else
+		cout << "Input Config file open FAILED." << endl;
+
+	int v = 0;
+	int b = 0;
+
+	FileInDes >> v;
+	FileInDes >> b;
+	_BLboundary = new point(v, b); 
+
+	FileInDes >> v;
+	FileInDes >> b;
+	_TRboundary = new point(v, b); 
+
+	/* Print Out for debuging*/
+	cout << _TRboundary->_x << " ";
+	cout << _TRboundary->_y << endl;
+	cout << _BLboundary->_x << " ";
+	cout << _BLboundary->_y << endl;
+
+	FileInDes.ignore(18);
+
+	while(!FileInDes.eof())
+	{
+		int p;
+		int w, x, y, z, q, r; 
+
+		FileInDes >> w >> x >> y >> z >> q >> r;
+		FileInDes.getline(temp, 100, '\n');
+		FileInDes >> p;
+
+		w = w - _BLboundary->_x; // Normalization
+		x = x - _BLboundary->_y;
+		y = y - _BLboundary->_x;
+		z = z - _BLboundary->_y;
+
+		block* Tmp = new block(w, x, y, z, q, r);
+		_Blocks.push_back(Tmp);
+		
+	}
+		
+	// cout << "DONE~~~~ Hohohohohohoho~~~" << endl;
+
+	FileInDes.close();
+
+////////////////////////////////////////////////////////////////////////////////////// Parsing Process file
+
+	fstream FileInPro;
+	FileInPro.open(processfile, ios::in);
+
+	FileInPro.getline(temp, 100, '\n');
+	FileInPro.getline(temp, 100, ' ');
+	FileInPro >> _window;
+	
+	 
+
+	FileInPro.close();
+
+////////////////////////////////////////////////////////////////////////////////////// Parsing rule file
+
+	fstream FileInRule;
+	FileInRule.open(rulefile, ios::in);
+
+	while(!FileInRule.eof())
+	{
+		int Cl, Wmin, Smin, Wmax;
+		float Dmin, Dmax;
+
+		FileInRule >> Cl;
+		FileInRule.getline(temp, 100, 'R');
+		FileInRule >> Wmin >> Smin >> Wmax >> Dmin >> Dmax;
+		rule* hi = new rule(Cl, Wmin, Smin, Wmax, Dmin, Dmax);
+		_Rules.push_back(hi);
+	}
+
+	FileInRule.close();
+
+	return;
+}
+
---
+++ circuit.h
@@ -0,0 +1,112 @@
+#ifndef CIRCUIT_H
+#define CIRCUIT_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <map>
+using namesapce std;
+
+class circuit 
+{
+public:
+	void parser(char* _filename);
+	void store_data();
+	point* _BLboundary; // Chip boundary
+	point* _TRboundary; // Chip boundary
+	vector<block*> _Blocks; // All the layout blocks
+	int _window; // Window size
+	vector<rule*> _Rules; // Conductor rules from rule file
+
+
+
+};
+
+class net
+{
+public:
+	vector<int> _critical; // Critical net ids;
+	vector<int> _power; // Power net ids;
+	vector<int> _ground; // Ground net ids;
+}
+
+class point
+{
+public:
+	int _x;
+	int _y;
+	point(int a, int b):_x(a), _y(b){}; // Constructor
+	point():_x(0), _y(0){}; // Constructor
+};
+
+class block
+{
+public:
+	point* _BL; // Bottom Left corner
+	point* _TR; // Top Right corner
+	int _id;
+	int _layer;
+	block(int w, int x, int y, int z, int p, int q)
+	{
+		_BL = new point(w, x);
+		_TR = new point(y, z);
+		_id = p;
+		_layer = q;
+	};
+};
+
+class cap
+{
+public:
+	float _f; // first
+	float _s; // second
+	cap(float x, float y):_f(x), _s(y){}; // Constructor
+	cap():_f(0), _s(0){}; // Constructor
+};
+
+class unit 
+{
+public:
+	float _rr; // range
+	cap* _cap;
+};
+
+class corr //Correlation
+{
+public:
+	string _name; // name
+	vector<*unit> _vu;
+	map<int, *unit> _mu;
+
+};
+
+class capacitance_table
+{
+public:
+	corr _array[][];
+
+};
+
+class rule
+{
+public:
+	int _Lid; // LayerID
+	int _wmin; // Minimum width
+	int _wmax; // Maximum width
+	int _smin; // Minimum space
+	float _dmin; // Minimum density
+	float _dmax; // Maximum dentity
+	rule(int a, int b, int c, int d, float e, float f)
+	{
+		_Lid = a;
+		_wmin = b;
+		_smin = c;
+		_wmax = d;
+		_dmin = e;
+		_dmax = f;
+	}; // Constructor
+};
+
+
+#endif
